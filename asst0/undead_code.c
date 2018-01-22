3d64cc472e46534da2923ac1746c67c03b9f8c85
PaulVorobyev <paul.vorobyev@gmail.com>
2018-01-22 07:45:56 -0500

    tabs -> spaces

--- asst0/undead_code.c
+++ asst0/undead_code.c
@@ -14,30 +14,30 @@
 
 void segment_fault_handler(int signum)
 {
-	printf("I am slain!\n");
+    printf("I am slain!\n");
 
-	//Use the signnum to construct a pointer to flag on stored stack
+    //Use the signnum to construct a pointer to flag on stored stack
     void * ptr  = &signum; //int ptr will have increments by 4, so use void ptr instead
 
-	//Increment pointer down to the stored PC
+    //Increment pointer down to the stored PC
     ptr += 0x3c; // distance to eip in prev frame according to gdb
 
-	//Increment value at pointer by length of bad instruction
+    //Increment value at pointer by length of bad instruction
     *(int *)ptr += 0x6; // JUMPing in gdb to the line after SIGSEGV gives a distance of 6 bytes
-	
-	
+    
+    
 }
 
 
 int main()
 {
-	int r2 = 0;
+    int r2 = 0;
 
-	signal(SIGSEGV, segment_fault_handler);
+    signal(SIGSEGV, segment_fault_handler);
 
-	r2 = *( (int *) 0 );
-	
-	printf("I live again!\n");
+    r2 = *( (int *) 0 );
+    
+    printf("I live again!\n");
 
-	return 0;
+    return 0;
 }
