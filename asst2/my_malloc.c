d3b87cba24e67f401fdf6b9dcee7be5b05149ac5
PaulVorobyev <paul.vorobyev@gmail.com>
2018-03-05 18:27:57 -0500

    Init some asst2 boilerplate

---
+++ asst2/my_malloc.c
@@ -0,0 +1,9 @@
+#include "my_malloc.h"
+
+
+void * mymalloc(size_t size, const char * file, int line, int flag) {
+    return (void *) 0;
+}
+
+void myfree(void * ptr, const char * file, int line, int flag) {
+}
---
+++ asst2/my_malloc.h
@@ -0,0 +1,19 @@
+/*
+ * File: my_malloc.h
+ * Author: Paul Vorobyev (pv149)
+ * Date: 03/04/2018
+ *
+ * Malloc definition
+ */
+
+#ifndef MY_MALLOC_H
+#define MY_MALLOC_HA
+
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+
+void * mymalloc(size_t size, const char * file, int line, int flag);
+void myfree(void * ptr, const char * file, int line, int flag);
+
+#endif
